e8d2d840252ffda0ddf54691a59123216b81f094
Patrick Stover <pstover@twitter.com>
2017-04-13 08:57:07 -0600

    Add src + tests + update convert.sh

--- bin/convert.sh
+++ bin/convert.sh
@@ -2,6 +2,14 @@
 
 # NOTE: You must put ffmpeg (https://ffmpeg.org/) on your path!
 
+# EXAMPLES:
+# Convert all wav files in /Users/pstover/workspace/school/csci5622/project/data/magnatagatune/extracted
+# and store them with matching relative paths in /Users/pstover/workspace/school/csci5622/project/data/magnatagatune/wav/test
+# bin/convert.sh /Users/pstover/workspace/school/csci5622/project/data/magnatagatune/extracted /Users/pstover/workspace/school/csci5622/project/data/magnatagatune/wav/test
+#
+# Convert the first 10 wav files in the directories above
+# bin/convert.sh /Users/pstover/workspace/school/csci5622/project/data/magnatagatune/extracted/b /Users/pstover/workspace/school/csci5622/project/data/magnatagatune/wav/test 10
+
 # Required arguments
 input_dir=$1
 output_dir=$2
---
+++ tests/audio_reader_test.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+from wavenet.audio_reader import AudioReader
+
+input_dir = '/Users/pstover/workspace/school/csci5622/project/data/magnatagatune/wav/first_5'
+coord = tf.train.Coordinator()
+reader = AudioReader(audio_dir=input_dir, coord=coord, sample_rate=100, gc_enabled=False, receptive_field=1)
+
+sample = reader.dequeue(1)
+print(sample)
